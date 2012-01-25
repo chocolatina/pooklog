3be3a5e5200a5c7327eb60cb79bc084d170f2e00
pplog <hogemoge@gmail.com>
2012-01-25 13:36:07 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pooklog::Application.config.secret_token = '3cde0b62255a4ef3e14f6b2b0e8b12751c7d8ddcb24379ed3804bcbac6ab85d14addda9517a67cbed8e0772388b68f17a666972af6f25a4a02f9caceafaed5bd'
